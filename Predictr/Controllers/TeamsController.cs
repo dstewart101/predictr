f64f892f89ba2af906f9e41852eedb554b8d54b9
David Stewart <sirdavethebrave@googlemail.com>
2018-05-09 09:53:13 +0100

    admins only can access teams, navigation tidied up

--- Predictr/Controllers/TeamsController.cs
+++ Predictr/Controllers/TeamsController.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Predictr.Data;
 using Predictr.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Predictr.Controllers
 {
+    [Authorize(Roles="Admin")]
     public class TeamsController : Controller
     {
         private readonly ApplicationDbContext _context;
