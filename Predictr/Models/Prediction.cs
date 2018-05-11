422777e914a86def7f73c9f78bac8c493ec2af69
David Stewart <sirdavethebrave@googlemail.com>
2018-05-11 09:52:29 +0100

    Prediction model changed to use ApplicationUser FK

--- Predictr/Models/Prediction.cs
+++ Predictr/Models/Prediction.cs
@@ -9,12 +9,13 @@ namespace Predictr.Models
     {
         public int Id { get; set; }
         public int FixtureId { get; set; }
-        public String ApplicationUser { get; set; }
+        public String ApplicationUserId { get; set; }
         public int HomeScore { get; set; }
         public int AwayScore { get; set; }
         public int? ModifierId { get; set; }
         public int Points { get; set; }
 
         public virtual Fixture Fixture { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
     }
 }
