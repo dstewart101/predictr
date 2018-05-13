ee0e657d2574f18586b0c80286974b9909d0890b
David Stewart <sirdavethebrave@googlemail.com>
2018-05-13 12:49:22 +0100

    changes to fixtures model and views

--- Predictr/Models/Fixture.cs
+++ Predictr/Models/Fixture.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,25 @@ namespace Predictr.Models
     public class Fixture
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:d MMM H:mm}")]
+        [Display(Name = "Date/Time")]
         public DateTime FixtureDateTime { get; set; }
+
+        [Display(Name = "Home")]
         public String Home { get; set; }
+
+        [Display(Name = "Home Score")]
         public int? HomeScore { get; set; }
+
+        [Display(Name = "Away")]
         public String Away { get; set; }
+
+        [Display(Name = "Away Score")]
         public int? AwayScore { get; set; }
         public String Result { get; set; }
+
+        [Display(Name = "Group/Stage")]
         public String Group { get; set; }
     }
 }
