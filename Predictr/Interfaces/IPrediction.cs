ae950bddc4b6d47a623aa27d4ee07b69de5174cc
David Stewart <sirdavethebrave@googlemail.com>
2018-05-22 16:11:33 +0100

    interface wip

---
+++ Predictr/Interfaces/IPrediction.cs
@@ -0,0 +1,12 @@
+﻿using Predictr.Models;
+using System.Collections;
+
+namespace Predictr.Interfaces
+{
+    public interface IPrediction
+    {
+        void Add(Prediction p);
+        void Edit(Prediction p);
+        IEnumerable GetPredictions();
+    }
+}
