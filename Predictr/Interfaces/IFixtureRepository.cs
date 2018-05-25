41b580cb9920e9ca7951957952f6c074c7f99934
David Stewart <sirdavethebrave@googlemail.com>
2018-05-25 16:04:05 +0100

    Fixtures Repository Pattern WIP

---
+++ Predictr/Interfaces/IFixtureRepository.cs
@@ -0,0 +1,17 @@
+﻿using Predictr.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Predictr.Interfaces
+{
+    public interface IFixtureRepository
+    {
+        Task<List<Fixture>> GetAll();
+        Task<Fixture> GetSingleFixture(int id);
+        void Add(Fixture fixture);
+        void Delete(Fixture fixture);
+        Task SaveChanges();
+    }
+}
---
+++ Predictr/Repositories/FixturesRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Predictr.Data;
+using Predictr.Interfaces;
+using Predictr.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Predictr.Repositories
+{
+    public class FixturesRepository : IFixtureRepository
+    {
+        private ApplicationDbContext _context;
+
+        public FixturesRepository(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        public Task<List<Fixture>> GetAll() => _context.Fixtures.ToListAsync();
+
+        public Task<Fixture> GetSingleFixture(int id) => _context.Fixtures.SingleOrDefaultAsync(f => f.Id == id);
+
+        public void Add(Fixture fixture) => _context.Add(fixture);
+
+        public Task SaveChanges() => _context.SaveChangesAsync();
+
+        public void Delete(Fixture fixture) => _context.Fixtures.Remove(fixture);
+    }
+}
