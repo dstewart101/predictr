6b3c93f56e9a3ff7d76a7bd08352b0db21cc1abe
David Stewart <sirdavethebrave@googlemail.com>
2018-06-03 23:38:06 +0100

    prediction calculations test added

---
+++ Predictr.Tests/Predictions/PredictionsTest.cs
@@ -0,0 +1,34 @@
+﻿using Predictr.Models;
+using Predictr.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Predictr.Tests.Predictions
+{
+    public class PredictionsTest
+    {
+
+        [Fact]
+        public async Task PredictionHandler_updatePredictions_awardsZeroForIncorrectResult()
+        {
+            // Arrange
+            Fixture fixture = new Fixture { Group = "B", FixtureDateTime = DateTime.Now, Home = "England", Away = "Belgium", HomeScore = 3, AwayScore = 2 };
+
+            List<Prediction> predictions = new List<Prediction>();
+
+            Prediction incorrectResult = new Prediction { Id = 1, FixtureId = fixture.Id, HomeScore = 1, AwayScore = 1 };
+
+            predictions.Add(incorrectResult);
+            var predictionHandler = new PredictionHandler(predictions, fixture);
+
+            // Act
+            var result = predictionHandler.updatePredictions();
+
+            // Assert
+            Assert.Equal(0, incorrectResult.Points);
+        }
+    }
+}
